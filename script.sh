574376edbd921fe06e22ccf239b8be49c4ec2a7a
Simon Abisoye <simon@hanabyte.com>
2024-03-05 15:24:56 -0500

    first commit

---
+++ script.sh
@@ -0,0 +1,13 @@
+
+set -e
+
+
+sudo apt-get update
+sudo apt-get install -y docker.io
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker ubuntu
+
+
+sudo apt-get autoremove -y
+sudo apt-get clean
